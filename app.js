7fa3a2fa52ede495de0522eabe6adfd813f72c21
Christian Valeri <mortlaxris@outlook.com>
2023-03-14 17:08:02 -0500

    Javascript after turn-change

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
 const gameBoard = () => {
     const playerX =  playerFactory("Player X", "0", "X");
     const playerO = playerFactory("Player O", "0", "O");
-    const currentPlayer = playerX.name;
+    let currentPlayer = playerX.name;
     const buttons = document.querySelectorAll(".button");
-    // const currentTurnSign;
-    // const currentTurnPlayer;
-    const gameBoardArr =["", "", "", "", "", "", "", "", ""];
+    const gameBoardArr = ["", "", "", "", "", "", "", "", ""];
 
     function playerFactory(name,points,sign){
         return{name,points,sign}
     }
-    function turns(){
-        if(currentPlayer === playerX){
-            squareSignClick();
-        }else{
-            squareSignClick();
-        }
 
-        //Need to switch between turn order
-        if(currentPlayer === playerX.name){
-            currentPlayer === playerO.name;
-        } else {
-            currentPlayer === playerX.name;
-        }
+    function turns(){
+      // switch players
+        currentPlayer = currentPlayer === playerX.name ? playerO.name : playerX.name;
     }
 
-    function squareSignClick(){
-        console.log("squareSignClick called");
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {
-              button.style.backgroundColor = 'blue'; // Change button background color to red
-            });
+    // add event listeners to buttons
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+        button.style.backgroundColor = 'blue';
+        gameBoardArr[index] = currentPlayer.sign;
+        turns();
         });
-    };
-    
+    });
 };
 gameBoard();
